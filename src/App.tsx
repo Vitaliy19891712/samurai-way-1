7942e708714bef7e8376522661580774cda0d17c
Vitaliy19891712 <stopkaiko@mail.ru>
2023-01-07 19:23:13 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./Components/Header";
+import { Navbar } from "./Components/Nav";
+import { Profile } from "./Components/Profile";
 
 function App() {
-  return <div className="App">Hello, samurai! Let's go!</div>;
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header className="header">
+      <img
+        src="https://png.pngtree.com/template/20191024/ourmid/pngtree-mountain-landscape-logo-design-hiking-travel-and-adventure-concept-design-image_323034.jpg"
+        alt="Logo"
+      />
+    </header>
+  );
+}
